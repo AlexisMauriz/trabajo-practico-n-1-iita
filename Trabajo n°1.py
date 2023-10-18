0936eed5e17ecd5a7dde4933c4a8d6e5dd528787
Ale <126083754+AlexisMauriz@users.noreply.github.com>
2023-10-18 18:11:41 -0300

    subiendo mi primer tp de python del iita.

---
+++ Trabajo n°1.py
@@ -0,0 +1,115 @@
+
+print ("-----Punto 1------")
+print("")
+Himno= "Bon Jovi"
+print("¿Cúal es tu banda favorita?: ", Himno)
+
+Himno = "It's my life de Bon Jovi"
+print("Decime tu canción favorita: ", Himno)
+
+print("")
+print ("-----Punto 2------")
+print("")
+
+
+N = "Alexis"
+P = "¿Te gustaría"
+A = "aprender a programar?"
+
+print (f"{N} {P} {A}")
+
+print("")
+print ("-----Punto 3------")
+print("")
+
+A = 4
+B = 4
+print (A + B)
+a = 10 
+b = 2
+d = "-"
+print (a - b)
+m = 2
+M = 4
+print (2*4)
+d = 16
+D = 2
+print (d/D)
+print("")
+print ("-----Punto 4------")
+print("")
+mi_entero = 7
+mi_decimal = 0.7
+mi_string = "Siete"
+mi_booleano = True
+print(type(mi_entero))
+print(type(mi_decimal))
+print(type(mi_string))
+print(type(mi_booleano))
+print("")
+print ("-----Punto 5------")
+print("")
+numero_decimal = float(input("Ingresa un número decimal: "))
+numero_entero = int(numero_decimal)
+print(f"El número decimal {numero_decimal} truncado es: {numero_entero}")
+print("")
+print ("-----Punto 6------")
+print("")
+variable1 = "1 != 2"
+print (1 != 2)
+variable2 = "True == 1"
+print (True == 1)
+variable3 = "False != False"
+print(False != False)
+variable4 = "False > 0"
+print(False > 0)
+variable5 = "1.0 < 1"
+print(1.0 < 1)
+variable6 = "test == test"
+print ("test" == "test")
+variable7 = "1.0 >= 1"
+print (1.0 >= 1)
+print("")
+print ("-----Punto 7------")
+print("")
+
+nombre = input("Por favor, ingresa tu nombre: ")
+print("")
+edad = int(input("Ingresa tu edad: "))
+print("")
+año_actual = 2023  
+año_cumplira_100 = año_actual + (100 - edad)
+print("")
+print(f"Hola, {nombre}. Cumplirás 100 años en el año {año_cumplira_100}.")
+print("")
+print("")
+print ("-----Punto 8------")
+print("")
+
+celsius = float(input("Ingresa la temperatura en grados Celsius: "))
+print("")
+fahrenheit = (9.0/5.0) * celsius + 32
+print(f"{celsius} grados Celsius equivale a {fahrenheit} grados Fahrenheit.")
+print("")
+print("")
+print ("-----Punto 9------")
+print("")
+numero1 = float(input("Ingresa el primer número: "))
+numero2 = float(input("Ingresa el segundo número: "))
+
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+potenciacion = numero1 ** numero2
+division_entera = numero1 // numero2
+
+
+print(f"{numero1} + {numero2} = {suma}")
+print(f"{numero1} - {numero2} = {resta}")
+print(f"{numero1} * {numero2} = {multiplicacion}")
+print(f"{numero1} / {numero2} = {division}")
+print(f"{numero1} ** {numero2} = {potenciacion}")
+print(f"{numero1} // {numero2} = {division_entera}")
+
+
